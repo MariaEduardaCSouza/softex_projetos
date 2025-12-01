8c380db2b26ca3a326c30ec2ce35194790bde25f
MariaEduardaCSouza <223533892+MariaEduardaCSouza@users.noreply.github.com>
2025-12-01 13:05:22 -0300

    exercicios mod 2

--- exércicios aula 23.09/nomes.py
+++
@@ -1,8 +0,0 @@
-
-class Pessoas:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
-
-    def __str__(self):
-        return f"{self.nome}, {self.idade} anos."
